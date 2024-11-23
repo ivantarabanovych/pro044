2ea2c95c400fdb80396583e3f62d00bcf1422c91
ivantarabanovych <vanjokcitet334@gmail.com>
2024-11-23 16:21:26 +0200

    q1

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const userName = "Ivan";
+const age = 25;
+const isStudent = true;
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet(userName));
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+const userName: string = "Ivan";
+const age: number = 25;
+const isStudent: boolean = true;
+
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet(userName));
